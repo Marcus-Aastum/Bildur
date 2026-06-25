5181bff057744b799a6b64b434b95a85439e0399
Marcus Aastum <marcusaastum@gmail.com>
2026-06-25 10:26:46 +0200

    Add migrator project

---
+++ Migrator/Migrations/001_AddImagesTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentMigrator;
+using FluentMigrator.SqlServer;
+
+namespace Migrator.Migrations;
+
+
+[Migration(001)]
+public class AddImagesTable : Migration
+{
+    public override void Up()
+    {
+        Execute.Sql(UpString);
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Images");
+    }
+
+    private string UpString = """
+        CREATE TABLE Images(
+        	ID INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+        	Content varbinary(MAX) NOT NULL,
+        	ImageType NVARCHAR(20) NOT NULL,
+        	Size INT,
+        	Width INT,
+        	Height INT
+        )
+        """;
+}
---
+++ Migrator/Program.cs
@@ -0,0 +1,59 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .AddUserSecrets(Assembly.GetAssembly(typeof(Program))!)
+                .Build();
+        
+        var serviceProvider = CreateServices(configuration.GetConnectionString("Bildur") ?? throw new ArgumentNullException("A connection string is required"));
+
+        // Put the database update into a scope to ensure
+        // that all resources will be disposed.
+        using (var scope = serviceProvider.CreateScope())
+        {
+            UpdateDatabase(scope.ServiceProvider);
+        }
+    }
+
+    /// <summary>
+    /// Configure the dependency injection services
+    /// </summary>
+    private static ServiceProvider CreateServices(string connectionString)
+    {
+        var serviceCollection = new ServiceCollection();
+        return new ServiceCollection()
+            // Add common FluentMigrator services
+            .AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb
+                // Add SQL Server support to FluentMigrator
+                .AddSqlServer()
+                // Set the connection string
+                .WithGlobalConnectionString(connectionString)
+                // Define the assembly containing the migrations
+                .ScanIn(typeof(Program).Assembly).For.Migrations())
+            // Enable logging to console in the FluentMigrator way
+            .AddLogging(lb => lb.AddFluentMigratorConsole())
+            // Build the service provider
+            .BuildServiceProvider(false);
+    }
+
+    /// <summary>
+    /// Update the database
+    /// </summary>
+    private static void UpdateDatabase(IServiceProvider serviceProvider)
+    {
+        // Instantiate the runner
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        // Execute the migrations
+        runner.MigrateUp();
+    }
+}
